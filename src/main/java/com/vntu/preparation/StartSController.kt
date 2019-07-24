7bb8cd10e78d5212080966a75cb0c17b03f0e445
Razakor <m.shevchuk.o@gmail.com>
2019-07-24 23:52:31 +0300

    realised student deletion and course transfiguration

--- src/main/java/com/vntu/preparation/StartSController.kt
+++ src/main/java/com/vntu/preparation/StartSController.kt
@@ -1,6 +1,8 @@
 package com.vntu.preparation
 
 import com.vntu.main.QueryResult
+import javafx.scene.control.Alert
+import javafx.scene.control.ButtonType
 import javafx.scene.control.DatePicker
 import javafx.scene.control.Label
 import java.sql.Date
@@ -9,7 +11,6 @@ import java.sql.SQLException
 class StartSController {
     lateinit var startDatePicker: DatePicker
     lateinit var endDatePicker: DatePicker
-
     private lateinit var startDate: Date
     private lateinit var endDate: Date
     private lateinit var label: Label
@@ -33,9 +34,33 @@ class StartSController {
         startDate = Date.valueOf(startDatePicker.value)
         endDate = Date.valueOf(endDatePicker.value)
         val query = "UPDATE timings\n" +
-                "SET start = '" + startDate + "', end = '" + endDate + "'\n" +
-                "WHERE id = '" + 1 + "'"
+                "SET start = '$startDate', end = '$endDate'\n" +
+                "WHERE id = '1'"
         QueryResult.updateDataBase(query)
         label.text = "Робота на виробництві починається: $startDate, закінчується: $endDate."
+    }
+
+    fun deleteAllStudents() {
+        val alert = Alert(Alert.AlertType.CONFIRMATION)
+        alert.title = "Видалення студентів"
+        alert.headerText = "Видалити всіх студентів?"
+        val result = alert.showAndWait()
+        if (result.get() == ButtonType.OK) {
+            QueryResult.updateDataBase("DELETE FROM student")
+            QueryResult.updateDataBase("DELETE FROM distribution")
+            QueryResult.updateDataBase("DELETE FROM `group`")
+        }
+    }
+
+    fun nextCourse() {
+        val alert = Alert(Alert.AlertType.CONFIRMATION)
+        alert.title = "Переведення студентів"
+        alert.headerText = "Перевести всіх студентів на наступний курс"
+        val result = alert.showAndWait()
+        if (result.get() == ButtonType.OK) {
+            val query = "UPDATE student\n" +
+                    "SET id_course = id_course + 1"
+            QueryResult.updateDataBase(query)
+        }
     }
 }
